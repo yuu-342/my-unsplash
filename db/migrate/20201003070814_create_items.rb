b14f7b9a04d64d53ad5c4f3ef2aa22dfef9c87cb
yuya <siren.night43@gmail.com>
2020-10-03 16:22:37 +0900

    アイテムモデル関連の実装

---
+++ db/migrate/20201003070814_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.references :category, index: true, foreign_key: true
+      t.string :note
+      t.string :image_path, null: false
+
+      t.timestamps
+    end
+  end
+end
